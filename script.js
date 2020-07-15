44d89a4a6d8ccf1a10c3b30586918acb577c7a11
Joe May <jmmay1974@gmail.com>
2020-07-14 22:04:27 -0700

    logic update

---
+++ script.js
@@ -0,0 +1,59 @@
+let queryTerm = ""
+let random1 = ""
+let random2 = ""
+let random3 = ""
+
+
+const charArr =[
+    "A-Bomb",
+"Abe Sapien",
+"Abin Sur",
+"Abomination",
+"Abraxas",
+"Absorbing Man",
+"Adam Monroe",
+"Adam Strange",
+"Agent 13",
+"Agent Bob"]
+
+for (let i=1; i < 4;){
+    if(queryTerm=""){
+        queryTerm = charArr[Math.floor(Math.random() * charArr.length)];
+        i++;
+     } else if (random1 !== queryTerm){
+         random1 = charArr[Math.floor(Math.random() * charArr.length)];
+         i++;
+     
+     } else if (random2 !== queryTerm && random2 !== random1){
+         random2 = charArr[Math.floor(Math.random() * charArr.length)];
+         i++;
+     }
+      else if (random3 !== queryTerm && random3 !== random1){
+         random3 = charArr[Math.floor(Math.random() * charArr.length)];
+         i++;
+     }
+     console.log(queryTerm);
+     console.log(random1);
+     console.log(random2);
+     console.log(random3);
+}
+
+// const queryTerm =""
+const APIKey = "134975468255420";
+const queryURL = "https://www.superheroapi.com/api.php/" + APIKey + "/search/" + queryTerm + "/image" 
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(returnData) {
+
+    const charImgURL = (returnData.results[0].image.url);
+    console.log(charImgURL);
+    const imageDiv = $('<div>');
+    const charImg = $('<img>');
+    charImg.attr('src', charImgURL);
+    imageDiv.append(charImg);
+
+    $('#image-holder').prepend(charImg);
+
+});  
