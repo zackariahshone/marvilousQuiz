d9ff2cebb949222ed83461c79cc9ca2c734633d1
Zack Shone <zackariahshone@gmail.com>
2020-07-16 19:26:21 -0700

    added hero

--- script.js
+++ script.js
@@ -16,12 +16,40 @@ $(document).ready(function () {
         if(x === 0){
             clearInterval(theTime);
         }
-        x --;
+        x--;
     }, 1000);
    }
-  
-  
+
    timer();
 
-  
+const queryTerm ="wolverine"
+const APIKey = "134975468255420";
+const queryURL = "https://www.superheroapi.com/api.php/" + APIKey + "/search/" + queryTerm + "/image"
+let y =0;
+   $.ajax({
+       
+       url: queryURL,
+       method: "GET",
+       
+       success: function (response) {
+           console.log(response);
+           //.hero-box
+           setInterval(() => {
+         
+            const randImg = $('img');
+            $('.hero-box').append(randImg);
+            const imgUrl = response.results[0].image.url;
+            y++;
+            if(y > 0){
+                y=0;
+            }
+          const hero = randImg.attr('src', imgUrl );
+          
+            
+          
+         }, 1000);
+
+       }
+   });
+
 });
