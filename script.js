2e4a53dbdaa75e963f0204e475f928839151b546
Zack Shone <zackariahshone@gmail.com>
2020-07-18 11:46:40 -0700

    timer does not work and new functionality to answer button and final button

--- script.js
+++ script.js
@@ -1,72 +1,89 @@
 $(document).ready(function () {
-
-
-   
+   $('#final').hide();
    let x;
    let h=0;
 
    function timer(){
-       x = 60;
-    
-   let theTime = setInterval(() => {
-       
-       $('.timer').text('Timer: ' + x);
-   
+//        x = 60;
+//    let theTime = setInterval(() => {
+//        $('.timer').text('Timer: ' + x);
+
         
-        if( x % 10 === 0 ){
-           // $('.hints-box').prepend("api hints");
-        }
-        if(x === 0){
-            clearInterval(theTime);
-        }
-        x--;
-    }, 1000);
+//         if( x % 10 === 0 ){
+//            // $('.hints-box').prepend("api hints");
+//         }
+//         if(x === 0){
+//             clearInterval(theTime);
+//         }
+//         x-1;
+//     }, 1000);
    }
 
-   timer();
+  
 
-const randChar = Math.floor(Math.random() * 61-1)
 
-const queryTerm = charArr[randChar];
-console.log(randChar);
-console.log(queryTerm);
-const APIKey = "134975468255420";
-const queryURL = "https://www.superheroapi.com/api.php/" + APIKey + "/search/" + queryTerm + "/image"
-let y =0;
-   $.ajax({
-       
-       url: queryURL,
-       method: "GET",
-       
-       success: function (response) {
-           console.log(response);
+
+function charGen(){
+    const randChar = Math.floor(Math.random() * 61-1)
+    timer();
+    const queryTerm = charArr[randChar];
+    console.log(randChar);
+    console.log(queryTerm);
+    const APIKey = "134975468255420";
+    const queryURL = "https://www.superheroapi.com/api.php/" + APIKey + "/search/" + queryTerm + "/image"
+    let y =0;
+    $.ajax({
+        
+        url: queryURL,
+        method: "GET",
+        
+        success: function (response) {
+            console.log(response);
            //.hero-box
            const hint = [
-           "Alias: " + response.results[0].biography.aliases,
-            "Alignment: " + response.results[0].biography.alignment,
-            "Alterego: " + response.results[0].biography.alterego,
-          //response.results[0].biography.aliases
-           ]
-           
+               "Alias: " + response.results[0].biography.aliases,
+               "Alignment: " + response.results[0].biography.alignment,
+               "Alterego: " + response.results[0].biography.alterego,
+              // "Conections: " +  response.results[0].biography.connections.group-affiliation,
+               "Publisher: " + response.results[0].biography.publisher
+               //response.results[0].biography.aliases
+            ]
+            
             const randImg = $('img');
             $('.hero-box').append(randImg);
             const imgUrl = response.results[0].image.url;
             $('#answer').text(response.results[0].name)
-            y++;
-            if(y > 0){
-                y=0;
-            }
-          const hero = randImg.attr('src', imgUrl );
-
-          setInterval(() => {
-             
            
-            $(".hints-box").append("<p>"+ hint[h] +"</p>"); 
-           // $('.hints-box').append(theHint);
-            h++;
-        }, 10000);
-          
-       }
-   });
+            const hero = randImg.attr('src', imgUrl );
+            
+            const hints = setInterval(() => {
+                // $(".hints-box").append("<p>"+ hint[h] +"</p>"); 
+                // h+1;
+                // if(h>5){
+                //     h=0;
+                //     clearInterval(hints);
+                // }
+            }, 1000*10);
+            
+        }
+    });
+}
+    
+    $('#start').click(function(){
+        $("#qNumber").text("HERO: 1/5");
+        charGen();
+        $('#start').hide();
+    });
+    let questionNum = 1;
+    $("#answer").click(function(){
+        charGen();
+        questionNum++;
+        $("#qNumber").text('HERO: ' + questionNum + "/5");
+        if(questionNum > 5){
+            $('#final').show();
 
+        }
+        
+    })
+    
 });
