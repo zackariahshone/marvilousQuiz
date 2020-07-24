d87ea5e661794c31343d5f15ecd4d6b114445e58
Zack Shone <zackariahshone@gmail.com>
2020-07-23 18:19:56 -0700

    big master commit

---
+++ FinalScore.js
@@ -0,0 +1,6 @@
+let finalScore = localStorage.getItem("score");  
+console.log(finalScore);          
+finalHolder = document.getElementById('finalScore');
+
+
+finalHolder.innerHTML = (finalScore);
